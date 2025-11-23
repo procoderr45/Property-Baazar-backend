20546fbd7d857c9f25252a63b81223b2fdcc5627
Om Takbhate <omtakbhate007@gmail.com>
2025-11-23 21:53:49 +0530

    feat(user): add user schema and model

---
+++ src/models/user.model.ts
@@ -0,0 +1,97 @@
+import mongoose from "mongoose";
+import { DbUser, userTypeValues } from "../types/user.type.js";
+
+const userSchema = new mongoose.Schema<DbUser>(
+    {
+        name: {
+            type: String,
+            required: [true, "Please provide your name"],
+            minLength: [3, "Name should be atleast 3 characters long"],
+            trim: true,
+        },
+        contact: {
+            countryCode: {
+                type: String,
+                required: [true, "Please provide your country code"],
+                trim: true,
+            },
+            mobile: {
+                type: Number,
+                required: [true, "Please provide your mobile number"],
+                unqiue: [true, "Mobile number already in use"],
+            },
+        },
+        email: {
+            type: String,
+            required: [true, "Please provide valid email id"],
+            unique: [true, "Email already in use"],
+        },
+        password: {
+            type: String,
+            required: [true, "Please provide password"],
+        },
+        bio: {
+            type: String,
+            default: "",
+            trim: true,
+        },
+        age: {
+            type: Number,
+        },
+        photoUrl: {
+            type: String,
+            default: "",
+            trim: true,
+        },
+        address: {
+            street: {
+                type: String,
+                trim: true,
+            },
+            city: {
+                type: String,
+                required: [true, "Please provide your city"],
+                trim: true,
+            },
+            pincode: {
+                type: Number,
+            },
+            state: {
+                type: String,
+                required: [true, "Please provide state"],
+                trim: true,
+            },
+            country: {
+                type: String,
+                required: [true, "Please provide country"],
+                trim: true,
+            },
+        },
+        isVerified: {
+            type: Boolean,
+            default: false,
+        },
+        isActive: {
+            type: Boolean,
+            default: true,
+        },
+        userType: {
+            type: String,
+            enum: {
+                values: userTypeValues,
+            },
+        },
+        socials: {
+            instagram: String,
+            youtube: String,
+            twitter: String,
+            linkedin: String,
+            facebook: String,
+        },
+    },
+    { timestamps: true }
+);
+
+const UserModel = mongoose.model<DbUser>("User", userSchema);
+
+export default UserModel;
