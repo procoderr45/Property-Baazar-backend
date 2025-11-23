14ecc11737fd0289a556b6374b38f1850f862caa
Om Takbhate <omtakbhate007@gmail.com>
2025-11-23 21:53:26 +0530

    feat(user): add Contact type for user entity

--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -3,12 +3,25 @@ import { Address } from "./address.type.js";
 
 export type UserType = "Buyer" | "Seller" | "Lawyer" | "Admin" | "Guest" | "Agent";
 
+export type Contact = {
+    countryCode: string;
+    mobile: number;
+};
+
 export const userTypeValues: UserType[] = ["Buyer", "Seller", "Lawyer", "Admin", "Guest", "Agent"];
 
 export interface User {
     name: string;
     email: string;
     password: string;
+    contact: Contact;
+    socials?: {
+        instagram?: string;
+        linkedin?: string;
+        twitter?: string;
+        youtube?: string;
+        facebook?: string;
+    };
     age?: number;
     photoUrl?: string;
     bio?: string;
