d0742ad65ae9e65c9c976836a5ef3ec32a40484c
Om Takbhate <omtakbhate007@gmail.com>
2026-01-22 11:00:43 +0530

    feat: Save amenties of a property

--- src/services/property.service.ts
+++ src/services/property.service.ts
@@ -1,14 +1,31 @@
+import AmenityModel from "../models/amenityType.model.js";
 import { propertyRepository } from "../repositories/property.repository.js";
-import { AddPropertyType, PropertyDoc, PropertyType } from "../types/property.type.js";
+import { AddPropertyType, PropertyDoc, PropertyType } from "../types/property/property.type.js";
 import { validateNewPropertyData } from "../utils/modules/property/property.utils.js";
 
 class PropertyService {
     async createProperty(postedBy: string, propertyData: AddPropertyType): Promise<PropertyType> {
 
         const validPropertyData = validateNewPropertyData(propertyData);
+        let propertyAmenityIds: string[] = [];
 
-        const newProperty = await propertyRepository.createProperty(postedBy, validPropertyData);
-        
+        for (let curAmenity of validPropertyData.amenities) {
+            if (propertyData.amenities.length > 0 && typeof propertyData.amenities[0] === "object") {
+                const amenity = new AmenityModel(curAmenity);
+                const amenityDoc = await amenity.save();
+
+                propertyAmenityIds.push(amenityDoc._id.toString());
+            }
+            else if(typeof curAmenity === "string") {
+                propertyAmenityIds.push(curAmenity);
+            }
+        }
+
+        const newProperty = await propertyRepository.createProperty(postedBy, {
+            ...validPropertyData,
+            amenities: propertyAmenityIds
+        });
+
         return newProperty;
     }
 
