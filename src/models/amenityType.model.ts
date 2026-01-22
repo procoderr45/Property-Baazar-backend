9cf81f1964d9fe78ecb7e3103abe02d29fba3914
Om Takbhate <omtakbhate007@gmail.com>
2026-01-22 10:59:57 +0530

    feat: create property amenity type and model

---
+++ src/models/amenityType.model.ts
@@ -0,0 +1,21 @@
+import mongoose, { Document } from "mongoose";
+import { AmenityType } from "../types/property/property.type.js";
+
+const amenityTypeSchema = new mongoose.Schema<AmenityType>({
+    title: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        unique: [true, "Amenity already exists. Select existing amenity"]
+    },
+    iconUrl: {
+        type: String,
+        default: "https://cdn-icons-png.freepik.com/512/18395/18395916.png?ga=GA1.1.1724462388.1765285011"
+    }
+}, {
+    timestamps: true,
+})
+
+const AmenityModel = mongoose.model<AmenityType>("AmenityType", amenityTypeSchema);
+
+export default AmenityModel;
---
+++ src/types/property/amenity.type.ts
@@ -0,0 +1,6 @@
+import mongoose from "mongoose";
+
+export type AmenityType = {
+    title: string;
+    iconUrl?: string;
+}
